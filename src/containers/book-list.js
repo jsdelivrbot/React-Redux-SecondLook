0d05fdbfe16cd6cde0ca7c77f53e1807c39619ea
Rickard <rickardkamel@gmail.com>
2017-01-11 12:50:22 +0100

    Learned about actionCreators / reducers.

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -50,4 +50,4 @@ function mapDispatchToProps(dispatch) {
 // Promote BookList from a component to a container - it needs to know
 // about this new dispatch method, selectBook. Make it available
 // as a prop
-export default connect(mapStateToProps, mapDispatchToProps())(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
