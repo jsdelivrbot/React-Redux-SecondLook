2d3e318767ec094f7021eac77ff345b0d33e2190
Rickard <rickardkamel@gmail.com>
2017-01-11 06:59:48 +0100

    Going over actions, reducers, containers...

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,12 @@
+/**
+ * Created by rickam on 2017-01-10.
+ */
+
+export default function () {
+    return [
+        {title: 'Javascript: The Good Parts'},
+        {title: 'Harry Potter'},
+        {title: 'The Dark Tower'},
+        {title: 'Eloquent Ruby'}
+    ]
+}
